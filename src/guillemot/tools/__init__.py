350c797914ca06f896fa114dae55b9eeacf15326
Matthew Evans <git@ml-evs.science>
2026-08-16 22:18:14 +0100

    Add tools for investigating possible files

--- src/guillemot/tools/__init__.py
+++ src/guillemot/tools/__init__.py
@@ -1,18 +1,31 @@
 from .topas import (
+    check_remote_topas_running,
     run_topas_refinement,
+    run_topas_refinement_remote,
     save_topas_inp,
+    RemoteRefinementResult,
     RunRefinementResult,
     SaveInpResult,
+    TopasProcessStatus,
+    TopasSSHConfig,
 )
 from .optimade import get_optimade_structures, print_structure, print_structures
+from .files import list_available_data, DataInventory
 from .plotting import plot_refinement_results
 
 __all__ = (
+    "check_remote_topas_running",
     "run_topas_refinement",
+    "run_topas_refinement_remote",
     "save_topas_inp",
+    "RemoteRefinementResult",
     "RunRefinementResult",
     "SaveInpResult",
+    "TopasProcessStatus",
+    "TopasSSHConfig",
     "get_optimade_structures",
     "print_structure",
     "print_structures",
+    "list_available_data",
+    "DataInventory",
 )
